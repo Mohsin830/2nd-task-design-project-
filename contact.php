b1fea782d4f8666be51de6f5c925582f60c38ac3
Mohsin830 <144918614+Mohsin830@users.noreply.github.com>
2023-09-18 11:44:45 +0500

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,394 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Contact Data desgin</title>
+
+</head>
+
+<body>
+    <div class="container-flud p-3 bg-info text-light">
+        <div class="row">
+            <nav class="navbar navbar-expand-lg">
+                <img class="ms-5" src="./images/bb.jpg" alt="" width="100px" height="60px">
+                <a class=" navbar-brand ms-3" href="#">Logo</a>
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <form class="col-4 fs-2 d-flex form-inline ms-2 me-3">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"><button
+                        class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <!-- <li class="nav-item active">
+                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        </li> -->
+                        <li class="nav-item fs-5">
+                            <a class="nav-link" href="About.php">About</a>
+                        </li>
+                        <li class="nav-item fs-5">
+                            <a class="nav-link" href="Contact.php">Contact</a>
+                        </li>
+                        <li class="nav-item fs-5">
+                            <a class="nav-link" href="Register.php">Register</a>
+                        </li>
+                        <li class="nav-item fs-5">
+                            <a class="nav-link" href="login.php">Login</a>
+                        </li>
+                        <li class="nav-item fs-5">
+                            <a class="nav-link" href="Sign up.php">Sign up</a>
+                        </li>
+                        <li class="nav-item fs-5">
+                            <a class="nav-link" href="Logout.php">Logout</a>
+                        </li>
+
+                    </ul>
+
+                </div>
+            </nav>
+        </div>
+    </div>
+
+    <div class="container mt-3">
+        <div class="row">
+            <div class=" col-3 bg-white p-3  mt-2 border border-danger">
+
+                <h5> Groceries & Pets</h5>
+                <h5> Health & Beauty</h5>
+                <h5> Men's Fashion</h5>
+                <h5> Women's Fashion</h5>
+                <h5> Mother & Baby</h5>
+                <h5> Home & Lifestyle</h5>
+                <h5> Electronic Devices</h5>
+                <h5> Electronic Accessories</h5>
+                <h5> TV & Home Appliances</h5>
+                <h5> Sports & Outdoor</h5>
+                <h5> Watches, Bags & Jewellery</h5>
+                <h5> Automotive & Motorbike</h5>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+
+            </div>
+
+
+            <div class="col-9 mt-2">
+                <div>
+                    <img src="./images/nnn.jpg" alt="" height="480px" width="843px">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container bg-primary text-center mt-3">
+        <div class="row">
+            <div class="col-12">
+                <h3 class="fs-2">Contact With us</h3>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem, harum!</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- 
+    <div class="container mt-3">
+        <div class="row">
+            <form class="form border border-danger">
+                <h2 class="text-center">Contact Data</h2>
+                <div class="">
+                    <p class="title">Contact</p>
+                    <p class="message">Signup now and get full access to our app. </p>
+                    <div class="flex">
+                        <label class="col-9">
+                            <input required="" placeholder="" type="text" class="input">
+                            <span>Name</span>
+                        </label>
+                    </div><br>
+                    <div>
+                        <label class="col-9">
+                            <input required="" placeholder="" type="email" class="input">
+                            <span>Email</span>
+                        </label>
+                    </div><br>
+                    <div>
+                        <label class="col-9">
+                            <input required="" placeholder="" type="password" class="input">
+                            <span>Password</span>
+                        </label>
+                    </div><br>
+                    <label class="col-9">
+                        <input required="" placeholder="" type="password" class="input">
+                        <span>C password</span>
+                    </label><br>
+                    <button class=" col-2 bg-info mt-4">Submit</button>
+                    <p class="signin fs-5">Already have an acount ? <a href="#">Signin</a> </p>
+            </form>
+        </div>
+    </div> -->
+
+
+
+                      <!-- <h2>Footer Apply For using in te fornted design.</h2> -->
+
+
+    <div class="container bg-info mt-5 p-4 text-dark">
+        <div class="row">
+            <div class="col-3">
+                <h4 class="text-danger">Customer Care</h4>
+                <h6>Help Center</h6>
+                <h6>How to Buy</h6>
+                <h6>Corporate & Bulk Purchasing</h6>
+                <h6>Returns & Refunds</h6>
+                <h6>Daraz Shop</h6>
+                <h6>Contact Us</h6>
+                <h6>Purchase Protection</h6>
+                <h6>Daraz Pick up Points</h6>
+                <h4 class="text-danger">Make Money With Us</h4>
+                <h6>Daraz University</h6>
+                <h6>Sell on Daraz</h6>
+                <h6>Join Daraz Affiliate Program</h6>
+            </div>
+
+            <div class="col-3">
+                <div class="ms-5">
+                    <h4 class="text-danger">Daraz</h4>
+                    <h6>About Us</h6>
+                    <h6>Digital Payments</h6>
+                    <h6>Daraz Donates</h6>
+                    <h6>Daraz Blog</h6>
+                    <h6>Terms & Conditions</h6>
+                    <h6>Privacy Policy</h6>
+                    <h6>NTN Number : 4012118-6</h6>
+                    <h6>STRN Number : 1700401211818</h6>
+                    <h6>Online Shopping App</h6>
+                    <h6>Online Grocery Shopping</h6>
+                    <h6>Daraz Exclusive</h6>
+                </div>
+            </div>
+
+            <div class="col-3">
+                <div class="ms-5">
+                    <h4 class="text-danger">Daraz International</h4>
+                    <a href="">Pakistan</a>
+                    <a href="" class="ms-5">Bangladesh</a>
+                    <a href="" class="mt-5">Srilanka</a>
+                    <a href="" class="ms-5">Myanmar</a>
+                    <a href="" class="mt-5">Nepal</a>
+                    <h4 class="text-danger">Payment Methods</h4>
+                    <h6>Jazzcash</h6>
+                    <h6>Easypaisa</h6>
+                    <h6>Hbl Bank</h6>
+                    <h6>Online payment</h6>
+                    <h6>Check cash</h6>
+
+                    <h4 class="text-danger">Verified by</h4>
+
+                    <h6>International Company</h6>
+                </div>
+            </div>
+
+            <div class="col-3">
+                <div class="ms-5">
+                    <h4 class="text-danger">Exclusive Deals and Offers!</h4>
+                    <img src="./images/pic1.jpg" alt="" height="140px" width="200px" class="mt-2 ms-3">
+                    <img src="./images/bb.jpg" alt="" height="140px" width="200px" class="mt-3 ms-3">
+                </div>
+            </div>
+        </div>
+
+        <div class="row mt-5">
+            <div class="col-3">
+                <h6>How Daraz Transformed Online Shopping in Pakistan</h6>
+                <p class="mt-4">
+                    Daraz first made waves in Pakistan’s e-commerce market after its introduction in 2012.
+                    We
+                    have since grown to become Pakistan’s largest platform for online shopping with a
+                    network
+                    spread across Asia in Pakistan, Bangladesh, Sri Lanka, Myanmar, and Daraz.com.np. Our
+                    vision
+                    was to provide a safe, efficient online marketplace platform for vendors and customers
+                    across the country to come together.
+                </p>
+                <h6>
+                    What Makes Us Different from Other Online Shopping Platforms?
+                </h6>
+                <p>Select from the Largest Online Marketplace in Pakistan
+                    With over 15 million products to select from, Daraz offers its customers the most
+                    comprehensive listing of products in the country. Whether you’re looking for
+                    electronics,
+                    apparel, appliances, or groceries – there is something for everyone.
+                </p>
+                <h6>
+                    Hassle Free Delivery
+                </h6>
+                <p>
+                    Online shopping is only as good as its execution and Daraz promises hassle free delivery
+                    right from the moment you order to when your package is dropped at your door. We cater
+                    to
+                    both major and smaller cities alike, and give you the choice to track your package as it
+                    makes its way to you so you always know your order status. If you are unsatisfied with
+                    any
+                    aspect of your order, we have a simple 7-day return or exchange policy.
+                </p>
+                <h6>Payment Options to Suit Every Style</h6>
+                <p>You can choose to pay through a credit/debit card, opt for cash on delivery or even go
+                    for
+                    EMI (easy monthly instalments).</p>
+                <p>You can choose to pay through a credit/debit card, opt for cash on delivery or even go
+                    for
+                    EMI (easy monthly instalments).</p>
+            </div>
+
+            <div class="col-3">
+                <div class="ms-5">
+                    <p>that offers you a secure, easy way to make payments. We also have easypaisa &
+                        jazzcash
+                        payment method for our customers' ease</p>
+                    <h6>
+                        Shop from Verified Vendors
+                    </h6>
+                    <p>
+                        Daraz understands that online shopping in Pakistan comes with its fair share of
+                        risks.
+                        This
+                        is why with Daraz Marketplace and Daraz Mall customers have the security of choosing
+                        from.
+
+                    </p>
+                    <h6>
+                        Shop Around the World with Daraz Global Collection
+                    </h6>
+                    <p>
+                        International sellers and local convenience come together with Daraz Global
+                        collection.
+                        Get
+                        the chance to shop online from vendors around the world without leaving the Daraz
+                        website.
+                        Featuring thousands of novelty gadgets and accessories, Daraz Global Collection
+                        offers
+                        you a
+                        selection of products that you won’t find anywhere else when you’re online shopping
+                        in
+                        Pakistan.
+                    </p>
+                    <h6>
+                        Avail Exclusive Discounts, Offers, and Promotions
+                    </h6>
+                    <p>
+                        Online shopping with Daraz means you get the chance to avail exclusive online-only
+                        promotional packages as well as discount vouchers from our vendors when you shop
+                        from
+                        their
+                        pages. Our flash sales give you customized product offers all curated with the help
+                        of
+                        our
+                        advanced AI technology so you always have deals you’ll actually be interested in!
+                    </p>
+                    <p>
+                        Online shopping with Daraz means you get the chance to avail exclusive online-only
+                        promotional packages as well as discount vouchers from our vendors.
+                    </p>
+
+                </div>
+            </div>
+
+            <div class="col-3">
+                <div class="ms-5">
+                    <h5>Top Categories & Brands</h5>
+                    <h6>MOBILE PHONES IN PAKISTAN</h6>
+                    <p>Apple iPhones, Honor Mobiles, Huawei Mobiles, Tecno Mobiles, Redmi Mobiles, Xiaomi Mi
+                        Mobiles, Nokia Mobiles, OnePlus Mobiles, Oppo Mobile Phones.
+                    </p>
+                    <h6>
+                        LATEST LAPTOPS
+                    </h6>
+                    <p>
+                        Dell Laptops, HP Laptops, Lenovo Laptops, Mouse, Gaming Graphic Cards, lenovo
+                        ideapad 3,
+                        Macbook Pro 13, Hp Probook 650 G2
+                    </p>
+                    <h6>LED TV</h6>
+                    <p>Changhong Led Tv, LG Led Tv, Samsung Led Tv, Sony Led Tv, TCL LED TVs</p>
+                    <h6>HOME APPLIANCES</h6>
+                    <h6>DSLR CAMERAS</h6>
+                    <p>Camera Tripods, Drones, IP & CCTV Cameras, Nikon D7000, Nikon D5600, Canon 200D,
+                        Canon
+                        1200D, Fujifilm Instax Mini 11, Canon M50</p>
+                    <h6>HEALTH & BEAUTY</h6>
+                    <p>sunisa foundation, Biofad, janssen facial kit, Glutathione Injection, Glutathione
+                        Cream
+                        Sauvage, Glutathione Tablets, Glutathione Soap.</p>
+                    <h6>TRENDING</h6>
+                    <p>June Shopping Expo 2023, Azaadi Sale 2022, Shopping Expo, 11 11 Sale, 12.12 Sale,
+                        Live
+                        Cricket Streaming, Online Bills, Core I5 Laptop, Gtx 1060, Samsung A32, Samsung A51,
+                        Samsung Z Flip, Tecno Camon 17, Tecno Spark 6, Vivo V20,
+                        Poco X3 Pro, Vivo V21, Vivo V21E, Vivo X70 Pro, Vivo Y12, Vivo Y12S, Vivo Y1S, Vivo
+                        Y20.
+                    </p>
+
+                    <h6>SHOP WORLDWIDE WITH LAZADA</h6>
+                    <p>Singapore, Malaysia, Philipines, Indonesia, Vietnam, Thailand</p>
+                    <div>
+                        <a href="" class="text-light">See more links</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-3">
+                <div class="ms-5">
+                    <h6>WOMEN'S FASHION</h6>
+                    <p>Al-Karam Studio, Warda, Salitex, Bonanza Satrangi, Edenrobe, Firdous, Junaid Jamshed,
+                        Limelight, Sana Safinaz, Mahru, Pushup Bra, Women Undergarments</p>
+                    <h6>MEN'S FASHION</h6>
+                    <p>Men's Shirts, Men's T-Shirts</p>
+                    <h6>ONLINE GROCERY STORE</h6>
+                    <p>Oil & Ghee, Basmati Rice, Dried Fruits, Chocolates, Mattresses</p>
+                    <h6>ONLINE BOOK STORE</h6>
+                    <p>English Books, Islamic Books, History Books, English Literature Books, Kids Urdu
+                        Stories,
+                        Pride & Prejudice, Harry Potter Story Books, Namal Novel, Nimra Ahmed Novels</p>
+                    <h6>USED CARS FOR SALE</h6>
+                    <p>Suzuki Wagon R, KIA Sportage, Honda City, Toyota Prado, Suzuki Alto, Suzuki Cultus,
+                        Honda
+                        Civic, Honda 125, Honda 70, Yamaha Ybr 125, Hi Speed 150, Hi Speed Freedom 200,
+                        Metro
+                        70,
+                        Super Power 125, Monster JMS 3500 Electric Bike, Super Power 70, Car Accessories</p>
+                    <h6>AIR CONDITIONERS</h6>
+                    <p>Kenwood Ac, Haier Ac, Gree Ac, Dawlance Ac, Orient Ac, Ecostar Ac, Inverex Solar Ac,
+                        Pel
+                        Ac
+                    </p>
+                    <h6>TOP MOBILE PHONES</h6>
+                    <p>Nokia G20, redmi 9, realme 7 pro, realme c15, realme c21, vivo y20s, realme c11 Price
+                        in
+                        Pakistan, itel vision 1 pro price in pakistan, samsung galaxy a02 price in pakistan,
+                        Infinix
+                        Hot 10, Infinix Hot 8, Infinix Hot 9, Infinix Note 10 Pro, Infinix Note 7, Infinix
+                        Note
+                        8,
+                        Infinix Zero 8, Inifnix Smart 5, iPhone 11, iPhone 12, iphone 12 Pro Max.iPhone 12
+                        Pro,
+                        iPhone 6, Itel A25, Mi 10T, Nokia 105, Oppo A15, Oppo A15S, Oppo A53, Oppo A54,
+                        Infinix.
+                    </p>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
